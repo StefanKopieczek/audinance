95549c16214286e285dd02fe3c8c0801e478eca3
Stefan Kopieczek <stefankopieczek@gmail.com>
2013-09-09 22:15:56 +0100

    Added tests and test templates to TestAbstractMultiplexer.

--- tests/com/stefankopieczek/audinancetests/conversion/multiplexers/TestAbstractMultiplexer.java
+++ tests/com/stefankopieczek/audinancetests/conversion/multiplexers/TestAbstractMultiplexer.java
@@ -1,9 +1,10 @@
 package com.stefankopieczek.audinancetests.conversion.multiplexers;
 
-import com.stefankopieczek.audinance.conversion.multiplexers.*;
 import com.stefankopieczek.audinance.audiosources.*;
+import com.stefankopieczek.audinance.conversion.multiplexers.*;
 import com.stefankopieczek.audinance.formats.*;
 import com.stefankopieczek.audinancetests.testutils.*;
+import org.junit.*;
 
 public abstract class TestAbstractMultiplexer
 {
@@ -42,4 +43,46 @@ public abstract class TestAbstractMultiplexer
 		                                      expectedAudio,
 											  audioOut);
 	}
+	
+	@Test
+	public void testSingleChannelIdentity()
+	{
+		double[] testChannel = {47.2, -7, 0.3, 9.8};
+		double[][] testData = {testChannel};
+		
+		assertOutputAsExpected("One-channel data plexed to one " +
+		                       "channel did not remain the same!",
+							   testData,
+							   1,
+							   testData);
+	}
+	
+	@Test
+	public void testMultiChannelIdentity()
+	{
+		int numChannels = 3;
+		double[][] testData = new double[numChannels][];
+		testData[0] = new double[]{4.4, 6.0, 0, 0.4, -3, 7};
+		testData[1] = new double[]{5.58, -9.99, -2, -7.3, 9.0, 5.9};
+		testData[2] = new double[]{-9, 3.2, 4.743, 5.8};
+		
+		assertOutputAsExpected("Multi-channel data plexed to the " +
+		                       "same number of channels did not " +
+							   "remain the same!",
+							   testData,
+							   numChannels,
+							   testData);
+	}
+	
+	@Test
+	public abstract void testMonoToStereo();
+	
+	@Test
+	public abstract void testStereoToMono();
+	
+	@Test
+	public abstract void plexUpWhenChannelEndsPrematurely();
+	
+	@Test
+	public abstract void plexDownWhenChannelEndsPrematurely();
 }
