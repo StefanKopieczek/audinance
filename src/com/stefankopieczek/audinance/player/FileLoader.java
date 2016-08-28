e4b80e384daff508c3ff9064d0da5282e96813f1
Stefan Kopieczek <stefankopieczek@gmail.com>
2016-08-28 22:25:37 +0100

    Build template UI
    Commit simple JFrame with console, play, and pause buttons.

--- src/com/stefankopieczek/audinance/player/FileLoader.java
+++ src/com/stefankopieczek/audinance/player/FileLoader.java
@@ -1,8 +1,37 @@
 package com.stefankopieczek.audinance.player;
 
+import com.stefankopieczek.audinance.formats.EncodedAudio;
+import com.stefankopieczek.audinance.formats.UnsupportedFormatException;
+import com.stefankopieczek.audinance.formats.wav.InvalidWavDataException;
+import com.stefankopieczek.audinance.formats.wav.WavData;
+
+import java.io.File;
+import java.io.IOException;
+
 /**
  * Created by stefan on 8/28/16.
  */
-public class FileLoader
+public final class FileLoader
 {
+    public static EncodedAudio loadAudio(File f) throws IOException
+    {
+        if (f.getName().endsWith(".wav"))
+        {
+            return new WavData(f);
+        }
+        else
+        {
+            // Speculatively try all the file types we know about.
+            try
+            {
+                return new WavData(f);
+            }
+            catch (InvalidWavDataException e)
+            {
+                // Looks like this isn't a WAV file.
+            }
+
+            throw new UnsupportedFormatException("File " + f + " is corrupt, or in an unsupported format");
+        }
+    }
 }
--- src/com/stefankopieczek/audinance/player/Player.java
+++ src/com/stefankopieczek/audinance/player/Player.java
@@ -1,5 +1,104 @@
 package com.stefankopieczek.audinance.player;
 
-public class Player
+import javax.imageio.plugins.jpeg.JPEGHuffmanTable;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Player extends JFrame
 {
+    public static final int WIDTH = 480;
+    public static final int HEIGHT = 580;
+
+    private JTextArea outputConsole;
+    private JButton loadButton;
+    private JButton playButton;
+
+    public Player()
+    {
+        init();
+    }
+
+    private void init()
+    {
+        setTitle("Audinance Player");
+        setSize(WIDTH, HEIGHT);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        outputConsole = buildOutputConsole();
+        loadButton = buildLoadButton();
+        playButton = buildPlayButton();
+        JPanel buttonPanel = buildButtonPanel(loadButton, playButton);
+
+        JPanel main = new JPanel();
+        main.setLayout(new BorderLayout());
+        main.add(outputConsole, BorderLayout.CENTER);
+        main.add(buttonPanel, BorderLayout.SOUTH);
+        add(main);
+    }
+
+    private void showLoadFileDialog()
+    {
+        // TODO
+    }
+
+    private void play()
+    {
+        // TODO
+    }
+
+    private static JTextArea buildOutputConsole()
+    {
+        JTextArea console = new JTextArea();
+        console.setEditable(false);
+        return console;
+    }
+
+    private JButton buildLoadButton()
+    {
+        JButton button = new JButton("Load file");
+        button.addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent)
+            {
+                Player.this.showLoadFileDialog();
+            }
+        });
+
+        return button;
+    }
+
+    private JButton buildPlayButton()
+    {
+        JButton button = new JButton("Play");
+        button.addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent)
+            {
+                Player.this.play();
+            }
+        });
+
+        return button;
+    }
+
+    private static JPanel buildButtonPanel(JButton... buttons)
+    {
+        JPanel buttonPanel = new JPanel();
+        for (JButton button : buttons)
+        {
+            buttonPanel.add(button);
+        }
+
+        return buttonPanel;
+    }
+
+    public static void main(String[] args)
+    {
+        JFrame player = new Player();
+        player.setVisible(true);
+    }
 }
