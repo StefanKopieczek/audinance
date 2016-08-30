b60c834596c908e0857a3e5f7a53dd0279d342af
Stefan Kopieczek <stefankopieczek@gmail.com>
2016-08-30 15:15:57 +0100

    Add FLAC support to the Player application
    ...even though FLAC decoding doesn't actually work yet.

--- src/com/stefankopieczek/audinance/player/FileLoader.java
+++ src/com/stefankopieczek/audinance/player/FileLoader.java
@@ -2,6 +2,8 @@ package com.stefankopieczek.audinance.player;
 
 import com.stefankopieczek.audinance.formats.EncodedAudio;
 import com.stefankopieczek.audinance.formats.UnsupportedFormatException;
+import com.stefankopieczek.audinance.formats.flac.FlacData;
+import com.stefankopieczek.audinance.formats.flac.InvalidFlacDataException;
 import com.stefankopieczek.audinance.formats.wav.InvalidWavDataException;
 import com.stefankopieczek.audinance.formats.wav.WavData;
 
@@ -25,6 +27,11 @@ public final class FileLoader
             sLogger.fine("Decided " + f + " is WAV-encoded, based on extension");
             return new WavData(f);
         }
+        else if (f.getName().endsWith(".flac"))
+        {
+            sLogger.fine("Decided " + f + " is FLAC-encoded, based on extension");
+            return new FlacData(f);
+        }
         else
         {
             // Speculatively try all the file types we know about.
@@ -40,6 +47,18 @@ public final class FileLoader
             {
                 // Looks like this isn't a WAV file.
                 sLogger.finest("Failed to decode " + f + " as WAV");
+            }
+
+            try
+            {
+                sLogger.finest("Trying to decode " + f + " as FLAC...");
+                FlacData flac = new FlacData(f);
+                flac.getDecodedAudio();
+            }
+            catch (InvalidFlacDataException e)
+            {
+                // Looks like this isn't a FLAC file.
+                sLogger.finest("Failed to decode " + f + " as FLAC");
             }
 
             throw new UnsupportedFormatException("File " + f + " is corrupt, or in an unsupported format");
