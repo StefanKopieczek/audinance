efd9de32c15a474458725850f45696a984048349
Stefan Kopieczek <stefankopieczek@gmail.com>
2019-03-02 23:49:54 +0000

    Style tweaks in AbstractMultiplexerTest

--- src/test/java/com/kopieczek/audinance/conversion/multiplexers/AbstractMultiplexerTest.java
+++ src/test/java/com/kopieczek/audinance/conversion/multiplexers/AbstractMultiplexerTest.java
@@ -11,65 +11,48 @@ public abstract class AbstractMultiplexerTest
 {
 	protected abstract Multiplexer getMultiplexer();
 	
-	protected void assertOutputAsExpected(String message,
-	                                      double[][] input, 
-	                                      int numChannels,
-	        				              double[][] expected)
-	{
-		DecodedSource[] channelsIn = new DecodedSource[input.length];
-		DecodedSource[] expectedChannels =
-		                           new DecodedSource[expected.length];
-		
-		for (int idx = 0; idx < input.length; idx++)
-		{
-			channelsIn[idx] = new MockDecodedSource(input[idx]);
+	protected void assertOutputAsExpected(String message, double[][] input, int numChannels, double[][] expectedOutput) {
+		AudioFormat dummyFormat = new AudioFormat(null, input.length);
+
+		DecodedSource[] inputChannels = new DecodedSource[input.length];
+		for (int idx = 0; idx < input.length; idx++) {
+			inputChannels[idx] = new MockDecodedSource(input[idx]);
 		}
-		
-		for (int idx = 0; idx < expected.length; idx++)
-		{
-			expectedChannels[idx] = 
-			                     new MockDecodedSource(expected[idx]);
+
+		DecodedSource[] expectedOutputChannels = new DecodedSource[expectedOutput.length];
+		for (int idx = 0; idx < expectedOutput.length; idx++) {
+			expectedOutputChannels[idx] = new MockDecodedSource(expectedOutput[idx]);
 		}
-		
-		AudioFormat format = new AudioFormat(5000, input.length);
-		DecodedAudio audioIn = new DecodedAudio(format, channelsIn);
-		DecodedAudio expectedAudio = 
-		                   new DecodedAudio(format, expectedChannels);
-		
+
+		DecodedAudio inputAudio = new DecodedAudio(dummyFormat, inputChannels);
+		DecodedAudio expectedOutputAudio = new DecodedAudio(dummyFormat, expectedOutputChannels);
+
 		Multiplexer multiplexer = getMultiplexer();
-		DecodedAudio audioOut = multiplexer.toNChannels(audioIn,
-		                                                numChannels);
+		DecodedAudio actualOutputAudio = multiplexer.toNChannels(inputAudio, numChannels);
 														
-		TestUtilities.assertDecodedAudioEqual(message,
-		                                      expectedAudio,
-											  audioOut);
+		TestUtilities.assertDecodedAudioEqual(message, expectedOutputAudio, actualOutputAudio);
 	}
 	
 	@Test
-	public void testSingleChannelIdentity()
-	{
+	public void testSingleChannelIdentity() {
 		double[] testChannel = {47.2, -7, 0.3, 9.8};
 		double[][] testData = {testChannel};
 		
-		assertOutputAsExpected("One-channel data plexed to one " +
-		                       "channel did not remain the same!",
+		assertOutputAsExpected("One-channel data plexed to one channel did not remain the same!",
 							   testData,
 							   1,
 							   testData);
 	}
 	
 	@Test
-	public void testMultiChannelIdentity()
-	{
+	public void testMultiChannelIdentity() {
 		int numChannels = 3;
 		double[][] testData = new double[numChannels][];
 		testData[0] = new double[]{4.4, 6.0, 0, 0.4, -3, 7};
 		testData[1] = new double[]{5.58, -9.99, -2, -7.3, 9.0, 5.9};
 		testData[2] = new double[]{-9, 3.2, 4.743, 5.8};
 		
-		assertOutputAsExpected("Multi-channel data plexed to the " +
-		                       "same number of channels did not " +
-							   "remain the same!",
+		assertOutputAsExpected("Multi-channel data plexed to the same number of channels did not remain the same!",
 							   testData,
 							   numChannels,
 							   testData);
