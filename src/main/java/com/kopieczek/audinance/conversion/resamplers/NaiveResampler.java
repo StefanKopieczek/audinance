94b83a8fae8d1d2af26fcef1c22cd5ab9157bb33
Stefan Kopieczek <stefankopieczek@gmail.com>
2019-03-03 13:01:44 +0000

    Style refactors in NaiveResampler

--- src/main/java/com/kopieczek/audinance/conversion/resamplers/NaiveResampler.java
+++ src/main/java/com/kopieczek/audinance/conversion/resamplers/NaiveResampler.java
@@ -9,111 +9,74 @@ import com.kopieczek.audinance.formats.InvalidAudioFormatException;
 import java.util.logging.Logger;
 
 /**
- * A <tt>Resampler</tt> that approximates points between known samples by
- * taking an (unweighted) average of its immediate neighbours.
- * This provides fast, "good enough" resampling, but is not as accurate as
- * polynomial interpolation.
- * It would be dramatically improved at little expense by using a weighted
- * average, but I'm tired and I need to write a ton more comments first.
- * 
- * @author Stefan Kopieczek
- *
+ * A resampler that approximates points between known samples by taking an (unweighted) average of its immediate neighbours.
  */
-public class NaiveResampler implements Resampler
-{
-    private static final Logger sLogger = Logger.getLogger(NaiveResampler.class.getName());
+public class NaiveResampler implements Resampler {
+    private static final Logger log = Logger.getLogger(NaiveResampler.class.getName());
 
-	public DecodedAudio resample(DecodedAudio original,
-                                 Integer targetSampleRate)
-	{
+	public DecodedAudio resample(DecodedAudio original, Integer targetSampleRate) {
 		DecodedSource[] originalChannels = original.getChannels();
 		AudioFormat originalFormat = original.getFormat();
 		Integer originalSampleRate = originalFormat.getSampleRate();
-        sLogger.info("Resampling " + original + " from " + originalSampleRate + "Hz to " +
-                     targetSampleRate + "Hz");
+        log.info("Resampling " + original + " from " + originalSampleRate + "Hz to " + targetSampleRate + "Hz");
 
-		AudioFormat newFormat = 
-			new AudioFormat(targetSampleRate, 
-			                originalFormat.getNumChannels());
+		AudioFormat newFormat = new AudioFormat(targetSampleRate, originalFormat.getNumChannels());
 		
-		DecodedSource[] newChannels = 
-	                	   new DecodedSource[originalChannels.length];
+		DecodedSource[] newChannels = new DecodedSource[originalChannels.length];
 	
-		// Build a new DecodedAudio object composed of resampled sources at the
-		// new frequency.
-		for (int ii = 0; ii < newChannels.length; ii++)
-		{
-			newChannels[ii] = new ResamplingSource(originalChannels[ii],
-			                                       originalSampleRate.intValue(),
-												   targetSampleRate.intValue());
+		// Build a new DecodedAudio object composed of resampled sources at the new frequency.
+		for (int ii = 0; ii < newChannels.length; ii++) {
+			newChannels[ii] = new ResamplingSource(originalChannels[ii], originalSampleRate, targetSampleRate);
 		}
 		
-		DecodedAudio result = new DecodedAudio(newChannels,
-		                                       newFormat);
-											   
-		return result;
+		return new DecodedAudio(newChannels, newFormat);
 	}
 	
 	/**
-	 * A <tt>DecodedSource</tt> that wraps another <tt>DecodedSource</tt> in
-	 * order to provide samples at the desired rate; that is, it is a 
-	 * resampling source for a single channel of audio.
-	 * It approximates samples that do not line up with the original data by
-	 * taking an unweighted average of the two immediate neighbour points.
-	 * 
-	 * @author Stefan Kopieczek
-	 *
+	 * Resampling source for a single channel of audio.
+     * Wraps the original DecodedSource channel, and performs resampling on the fly.
+	 * Approximates samples that do not line up with the original data by* taking an unweighted average of the two
+	 * immediate neighbour points.
 	 */
-	private class ResamplingSource extends DecodedSource
-	{
-		/**
-		 * The original audio data.
-		 */
-		private final DecodedSource mOriginal;
+	private static class ResamplingSource extends DecodedSource {
+		private final DecodedSource originalChannel;
 		
 		/**
-		 * The factor by which the sample rate is to be divided to get from the
-		 * original to the target.
+         * The ratio of original sample rate to desired sample rate.
+		 * For example, if the original rate is 20kHz, and the desired rate is 40Khz, the scale factor is 0.5.
 		 */
-		private final float mScaleFactor;
+		private final float scaleFactor;
 		
 		/**
 		 * The tolerance we use for assessing equality of doubles.
-		 * This is used to judge if two instants in time are equal, so that we
-		 * can re-use points in the original data that are close to the desired
-		 * sample points.
+		 * This is used to judge if two instants in time are equal, so that we can re-use points in the original data
+		 * that are close to the desired sample points.
 		 */
 		private static final float IDX_TOLERANCE = 0.0001f;
 		
-		public ResamplingSource(DecodedSource original,
-		                        int originalSampleRate,
-								int targetSampleRate)
-		{
-			mOriginal = original;
-			mScaleFactor = originalSampleRate * 1.0f / targetSampleRate;			
+		public ResamplingSource(DecodedSource originalChannel, int originalSampleRate, int targetSampleRate) {
+			this.originalChannel = originalChannel;
+			scaleFactor = originalSampleRate * 1.0f / targetSampleRate;
 		}
 		
 		@Override
-		public double getSample(int idx) 
-				throws NoMoreDataException, InvalidAudioFormatException
-		{
-			float floatIdx = idx * mScaleFactor;
+		public double getSample(int idx) throws NoMoreDataException, InvalidAudioFormatException {
+			float floatIdx = idx * scaleFactor;
 			
-			if (Math.abs(floatIdx - idx) < IDX_TOLERANCE)
-			{
-				return mOriginal.getSample(idx);
+			if (Math.abs(floatIdx - idx) < IDX_TOLERANCE) {
+				// We're basically on top of an existing sample - just return it
+				return originalChannel.getSample(idx);
 			}
-			
-			double precursor = mOriginal.getSample((int)floatIdx);
-			double successor = mOriginal.getSample((int)Math.ceil(floatIdx));
-			
-			return (precursor+successor)/2;
+
+			// We're between two samples. Return their average.
+			double precursor = originalChannel.getSample((int)floatIdx);
+			double successor = originalChannel.getSample((int)Math.ceil(floatIdx));
+			return (precursor + successor) / 2;
 		}
 		
 		@Override
-		public int getNumSamples()
-		{
-			return (int)(mOriginal.getNumSamples() / mScaleFactor);
+		public int getNumSamples() {
+			return (int)(originalChannel.getNumSamples() / scaleFactor);
 		}
 	}
 }
